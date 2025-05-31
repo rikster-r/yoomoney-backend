76690dfe1f07e4c4a89bc041c518aaec7407ce79
rikster-r <seitdanial43@gmail.com>
2025-06-01 00:22:58 +0500

    Update env

--- global.d.ts
+++ global.d.ts
@@ -1,12 +1,10 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      MONGODB_URI: string;
-      JWT_SECRET: string;
-      CLOUDINARY_CLOUD_NAME: string;
-      CLOUDINARY_API_KEY: string;
-      CLOUDINARY_API_SECRET: string;
-      CLOUDINARY_URL: string;
+      SHOP_ID: string;
+      SECRET_KEY: string;
+      SUPABASE_URL: string;
+      SUPABASE_KEY: string;
     }
   }
 }
